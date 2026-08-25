efcffe7906d31f39d08fbd54ab42bf576c9b6e2a
Takashi SASAKI <takashi316@gmail.com>
2026-08-25 11:31:32 +0900

    Pin remote Composition installer to S5

--- scripts/install_composition_skill.py
+++ scripts/install_composition_skill.py
@@ -14,7 +14,7 @@ from pathlib import Path, PurePosixPath
 from typing import BinaryIO, Protocol
 
 TOOLCHAIN_REPOSITORY = "TakashiSasaki/templates"
-SKILL_SOURCE_REVISION = "8f8c36aa4bdf3060f9217edde644856d1da9d72d"
+SKILL_SOURCE_REVISION = "f9508b92f5b7835fa1af9741f3941f32f6d3db28"
 FULL_SHA = re.compile(r"^[0-9a-f]{40}$")
 ARCHIVE_LIMIT = 16 * 1024 * 1024
 SKILL_LIMIT = 8 * 1024 * 1024
